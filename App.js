9b032d65fe2e3698c91cb3acaab21ed73c1135df
anas12136 <kaarwaa@gmail.com>
2023-10-19 21:58:16 +0500

    Navigation Set

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreens from './screens/HomeScreens';
 
 
+const Stack = createNativeStackNavigator()
 // Tailwind css in react native
 // https://medium.com/@simpleandshort/how-to-use-nativewind-tailwindcss-in-your-react-native-application-67874f41e13f 
 export default function App() {
   return (
-    <View >
-      <Text className="text-red-500">Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={HomeScreens}/>
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/HomeScreens.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeScreens = () => {
+  return (
+    <View>
+      <Text className="text-red-600 text-center mt-8">HomeScreens</Text>
+    </View>
+  )
+}
+
+export default HomeScreens
